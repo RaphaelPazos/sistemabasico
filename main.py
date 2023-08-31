d72717f2e09c91fb863c65e02cf9336c2272a54f
Nosmaritimos <raphaelpazoslc@gmail.com>
2023-08-31 17:20:08 -0300

    extract lista de opções

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from bs4 import BeautifulSoup
 import requests
 
 url = 'https://opcoes.net.br/login'
-values = {'CPF': '016.773.836-40',
-          'Password': 'maerskline2008'}
-
+values = {'CPF': '',
+          'Password': ''}
+r = requests.post(url, data=values)
+opt = requests.get("https://opcoes.net.br/opcoes/bovespa/vencimentos-longos")
+sopa = BeautifulSoup(opt.content,"html.parser")
+sopaespecifica = sopa.find("div", {"id":"area-lista-opcoes"})
 
 if __name__ == "__main__":
-    r = requests.post(url, data=values)
-    print(r.content)
+    print(sopaespecifica)
